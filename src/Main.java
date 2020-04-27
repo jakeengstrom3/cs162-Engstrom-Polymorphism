76d3e6acff386db096916d764d6263825e3963c8
Jacob Engstrom <jakeengstrom3@gmail.com>
2020-04-26 18:38:46 -0700

    Added data strucutre to handle printing each employee type

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args){
-        TechnicalWriter technicalWriter = new TechnicalWriter("Mark", 50000, 4, 2);
-        System.out.println(technicalWriter.toString());
-        System.out.println();
-        Engineer engineer = new Engineer("Natashia", 50000, 7, 2);
-        System.out.println(engineer.toString());
-        System.out.println();
-        ProductManager productManager = new ProductManager("Carlos", 50000, 8, 5);
-        System.out.println(productManager.toString());
-        System.out.println();
+        ArrayList<Employee> employees = new ArrayList<Employee>();
+
+        employees.add(new TechnicalWriter("Mark", 50000, 4, 2));
+        employees.add(new Engineer("Natashia", 50000, 7, 2));
+        employees.add(new ProductManager("Carlos", 50000, 8, 5));
+
+        for(Employee e : employees){
+            System.out.println(e);
+            System.out.println();
+        }
+
     }
 }
